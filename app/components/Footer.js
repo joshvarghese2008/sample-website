6d74cb7742894d7faaa2b57107fbb7f4582279d3
Josh Varghese <2008mlpy@gmail.com>
2024-12-18 15:23:24 +1100

    Added Footer

--- app/components/Footer.js
+++ app/components/Footer.js
@@ -1,7 +1,34 @@
+'use client'
+
+import Link from "next/link"
+import styles from "./components.module.css"
+import { FacebookRounded } from "@mui/icons-material"
+import { YouTube } from "@mui/icons-material"
+
 export default function Footer() {
     return (
-        <div style={{height: 500}}>
-            Hello
+        <div className={styles.footer}>
+            <div className={styles.footercentre}>
+                <div className={styles.footercontent}>
+                    <div className={styles.footerlinks}>
+                        <div className={styles.footerwebsitelinkscontainer}>
+                            <Link href={'/'} className={styles.footerwebsitelink}>Home</Link>
+                            <Link href={'/'} className={styles.footerwebsitelink}>Our Services</Link>
+                            <Link href={'/'} className={styles.footerwebsitelink}>What We Believe</Link>
+                            <Link href={'/'} className={styles.footerwebsitelink}>Give</Link>
+                            <Link href={'/'} className={styles.footerwebsitelink}>Plan A Visit</Link>
+                        </div>
+                        <div className={styles.footerexternallinkscontainer}>
+                            <FacebookRounded />
+                            <YouTube />
+                        </div>
+                    </div>
+                    <hr className={styles.footerdivider} />
+                    <div className={styles.footercopyright}>
+                        <p>&copy; 2025 PCA Church. All rights reserved</p>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
